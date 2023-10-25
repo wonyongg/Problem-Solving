f4ec83561c1b21da45c72bf6dc152a25e8d0d221
wonyongg <111210881+wonyongg@users.noreply.github.com>
2023-10-26 02:24:33 +0900

    11720

--- baekjoon/src/main/java/class1/baekjoon11720.java
+++ baekjoon/src/main/java/class1/baekjoon11720.java
@@ -1,9 +1,11 @@
+package class1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+public class baekjoon11720 {
 
-public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -21,6 +23,3 @@ public class Main {
         System.out.println(sum);
     }
 }
-/**
- * 통과 후 다음 사용을 위해 삭제!!
- */
