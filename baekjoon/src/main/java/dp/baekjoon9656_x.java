fb572ff354e35515b562ab0cdc6317594dabff69
wonyongg <111210881+wonyongg@users.noreply.github.com>
2024-02-22 02:29:32 +0900

    9656

---
+++ baekjoon/src/main/java/dp/baekjoon9656_x.java
@@ -0,0 +1,27 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon9656_x {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+        for (int i = 4; i <= N; i++) {
+            dp[i] = Math.min(dp[i-1],dp[i-3]) + 1;
+        }
+        if (dp[N] % 2 == 0) {
+            System.out.print("SK");
+        } else {
+            System.out.print("CY");
+        }
+    }
+}
