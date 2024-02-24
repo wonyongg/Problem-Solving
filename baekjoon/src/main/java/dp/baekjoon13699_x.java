704fc42155fcfb48850172d65b382e35da1b8574
wonyongg <111210881+wonyongg@users.noreply.github.com>
2024-02-24 13:04:30 +0900

    13699

--- baekjoon/src/main/java/dp/baekjoon13699_x.java
+++ baekjoon/src/main/java/dp/baekjoon13699_x.java
@@ -4,18 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class baekjoon13699 {
+public class baekjoon13699_x {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] t = new int[36];
+        long[] t = new long[36];
         t[0] = 1;
         t[1] = 1;
-        for (int i = 2; i <= 35; i++) {
-            t[i] = t[i-1] + t[i-1] * t[0];
+        for (int j = 2; j < 36; j++) {
+            for (int i = 0; i < j; i++) {
+                t[j] += t[i] * t[j-1-i];
+            }
         }
 
         System.out.println(t[n]);
@@ -24,5 +26,6 @@ public class baekjoon13699 {
 
 // t(0) = 1
 // t(1) = t(0) * t(1-1) = t(0) * t(0)
-// t(2) = t(0) * t(1-1) + t(1) * t(2-2) = t(0) * t(0) + t(1) * t(0)
-// t(2) = t(0) * t(1-1) + t(1) * t(2-2) + t(2) * t(3-3) = t(0) * t(0) + t(1) * t(0) + t(2) * t(0)
+// t(2) = t(0) * t(2-1) + t(1) * t(1-1) = t(0) * t(1) + t(1) * t(0)
+// t(3) = t(0) * t(3-1) + t(1) * t(2-1) + t(2) * t(1-1) = t(0) * t(2) + t(1) * t(1) + t(2) * t(0)
+//                                                        t(0) *
